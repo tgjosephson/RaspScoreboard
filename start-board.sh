a460e2e762c94a83ee8410a32b6324d8c3fffedc
Thomas Josephson <tgjosephson@gmail.com>
2023-08-23 07:42:55 -0400

    logic to run mlb board continuously

--- start-board.sh
+++ start-board.sh
@@ -1,8 +1,28 @@
 echo "starting board"
 python3 main.py
-git clone https://github.com/MLB-LED-Scoreboard/mlb-led-scoreboard.git
 cd mlb-led-scoreboard/
-sudo ./install.sh
+
+currently_running=false
+while :;
+do
+  current_date_time=$(date +%H%M)
+  echo "Current date and time: $current_date_time"
+  echo "Top currently_running: $currently_running"
+  if [ "$current_date_time" -ge 0700 -a "$current_date_time" -le 2200 ]; then
+    if [ "$currently_running" = false ]; then
+      echo "Starting main.py"
+      sudo python3 main.py --led-gpio-mapping="adafruit-hat" --led-brightness 30 &
+      currently_running=true
+    fi
+  elif [ "$currently_running" = true ]; then
+    echo "Killing main.py"
+    sudo pkill -f main.py
+    currently_running=false
+  fi
+  echo "Bottom currently_running: $currently_running"
+  sleep 30
+done
+
 echo "end"
 sleep 5
 exit 0
