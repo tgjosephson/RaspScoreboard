7fd6a899c5e979dbc8e5c453379fd1553b2a037b
Thomas Josephson <tgjosephson@gmail.com>
2023-08-23 07:31:53 -0400

    cloning mlb repo and install to startup

--- start-board.sh
+++ start-board.sh
@@ -1,5 +1,8 @@
-echo "start"
+echo "starting board"
 python3 main.py
+git clone https://github.com/MLB-LED-Scoreboard/mlb-led-scoreboard.git
+cd mlb-led-scoreboard/
+sudo ./install.sh
 echo "end"
 sleep 5
 exit 0
