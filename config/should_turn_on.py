83fbb6eb6477e594c67c272ab3271776ec1d6804
Thomas Josephson <tgjosephson@gmail.com>
2023-08-26 10:41:48 -0400

    weekend/weekday config

--- config/should_turn_on.py
+++ config/should_turn_on.py
@@ -4,11 +4,19 @@ from datetime import datetime
 config_file = open('./config/board-config.json')
 config = json.load(config_file)
 
+# Overall power
 if config['power'] == 'OFF':
     print("False")
     exit(0)
 
-for time_window in config['display_times']:
+# Display times
+weekno = datetime.today().weekday()
+if weekno < 5:
+    display_times = config['display_times']['weekday']
+else:  # 5 Sat, 6 Sun
+    display_times = config['display_times']['weekend']
+
+for time_window in display_times:
     current_time = int(datetime.now().strftime("%H%M"))
     if current_time >= time_window[0] and current_time <= time_window[1]:
         print("True")
