19536783cc35f9df7041eafc5336a05ac66b18e3
tgjosephson <58780944+tgjosephson@users.noreply.github.com>
2023-08-29 07:24:24 -0400

    Update custom.py

--- config/custom.py
+++ config/custom.py
@@ -22,5 +22,5 @@ class Custom:
         for game in self.preferred_games:
             game_time = datetime.strptime(game["game_datetime"], '%Y-%m-%dT%H:%M:%Sz')
             timedelta = game_time - current_time
-            if timedelta.days < 0 or (timedelta.seconds)/60 < 180:
+            if timedelta.days < 0 or (timedelta.seconds)/60 < 60:
                 return True
