4d4d925da1868b2345ce642cd50e70605a0b21fa
Thomas Josephson <tgjosephson@gmail.com>
2023-10-06 14:21:50 -0400

    fix start-board

--- start-board.sh
+++ start-board.sh
@@ -9,10 +9,10 @@ do
   if [ "$(python3 ./should_turn_on.py)" = "True" ]; then
     if [ "$currently_running" = false ]; then
       echo "Starting board"
-      if mode_setting == 'nhl'; then
+      if [ "$mode_setting" == 'nhl' ]; then
         cd nhl_led_scoreboard/
         sudo python3 src/main.py --led-gpio-mapping="adafruit-hat" --led-slowdown-gpio=2 &
-      elif mode_setting == 'mlb'; then
+      elif [ "$mode_setting" == 'mlb' ]; then
         cd mlb_led_scoreboard/
         sudo python3 main.py --led-gpio-mapping="adafruit-hat" --led-brightness 30 &
       fi
@@ -21,9 +21,9 @@ do
     fi
   elif [ "$currently_running" = true ]; then
     echo "Killing board"
-    if mode_setting == 'nhl'; then
+    if [ "$mode_setting" == 'nhl' ]; then
       sudo pkill -f src/main.py
-    elif mode_setting == 'mlb'; then
+    elif [ "$mode_setting" == 'mlb' ]; then
       sudo pkill -f main.py
     fi
     sudo reboot
