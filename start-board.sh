2d0264f3d73845cf6c3767c1b62726d9fc6e446c
Thomas Josephson <tgjosephson@gmail.com>
2023-10-06 14:04:11 -0400

    config for nhl vs mlb

--- start-board.sh
+++ start-board.sh
@@ -1,20 +1,30 @@
 echo "starting board"
 
+config_file = open('./config/board-config.json')
+config = json.load(config_file)
+
 currently_running=false
 while :;
 do
   echo "Top currently_running: $currently_running"
   if [ "$(python3 ./should_turn_on.py)" = "True" ]; then
     if [ "$currently_running" = false ]; then
-      echo "Starting main.py"
-      cd nhl_led_scoreboard/
-      sudo python3 src/main.py --led-gpio-mapping="adafruit-hat" --led-slowdown-gpio=2 &
+      echo "Starting board"
+      if config['mode'] == 'nhl':
+        cd nhl_led_scoreboard/
+        sudo python3 src/main.py --led-gpio-mapping="adafruit-hat" --led-slowdown-gpio=2 &
+      elif config['mode'] == 'mlb':
+        cd mlb_led_scoreboard/
+        sudo python3 main.py --led-gpio-mapping="adafruit-hat" --led-brightness 30 &
       cd ..
       currently_running=true
     fi
   elif [ "$currently_running" = true ]; then
-    echo "Killing main.py"
-    sudo pkill -f src/main.py
+    echo "Killing board"
+    if config['mode'] == 'nhl':
+      sudo pkill -f src/main.py
+    elif config['mode'] == 'mlb':
+      sudo pkill -f main.py
     sudo reboot
     currently_running=false
   fi
